6d71cdf8772bd3b42d91aea8e2e308549d074a75
ahmetgor <ahmet.gor@i2isystems.local>
2017-12-13 11:30:31 +0300

    commit

--- server.js
+++ server.js
@@ -13,16 +13,19 @@ mongoose.connect(databaseConfig.url);
 app.listen(process.env.PORT || 8080);
 console.log("App listening on port 8080");
 
-app.use(bodyParser.urlencoded({ extended: false })); // Parses urlencoded bodies
-app.use(bodyParser.json()); // Send JSON responses
+app.use(bodyParser.urlencoded({ extended: false, limit: '10mb' })); // Parses urlencoded bodies
+app.use(bodyParser.json({limit: '10mb'})); // Send JSON responses
 app.use(logger('dev')); // Log requests to API using morgan
 app.use(cors());
 
 app.use(function(req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
-   res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  //  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   res.header('Access-Control-Allow-Origin', "http://localhost");
+   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+   res.header('Access-Control-Allow-Headers', 'Content-Type');
    next();
 });
- 
+
 router(app);
